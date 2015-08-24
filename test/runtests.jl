3833345328221d568706d3df5db13a6574168402
Oleksandr Motsak <http://goo.gl/mcpzY>
2015-08-24 19:59:07 +0000

    Fix wrong Libdl.DL_LOAD_PATH for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,9 @@ using Base.Test, Nemo
 # write your own tests here
 
 pkgdir = Pkg.dir("Nemo")
-pwd = "$pkgdir/src"
-push!(Libdl.DL_LOAD_PATH, "$pwd/../src/lib")
+
+# pwd = "$pkgdir/src"
+# push!(Libdl.DL_LOAD_PATH, "$pwd/../src/lib")
+push!(Libdl.DL_LOAD_PATH, joinpath(pkgdir, "local", "lib"))
 
 Nemo.Test.test_all()
