af3cb0660e768efc76c97e238a680c6ffd1e8dab
Tommy Hofmann <thofma@gmail.com>
2018-03-12 17:28:03 +0100

    Bump antic version and wrap representation_matrix

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -161,6 +161,7 @@ end
 const libmpfr = joinpath(pkgdir, "local", "lib", "libmpfr")
 const libflint = joinpath(pkgdir, "local", "lib", "libflint")
 const libarb = joinpath(pkgdir, "local", "lib", "libarb")
+const libantic = joinpath(pkgdir, "local", "lib", "libantic")
 
 function flint_abort()
   error("Problem in the Flint-Subsystem")
@@ -276,6 +277,7 @@ function __init__()
        Libdl.dlopen(libmpfr)
        Libdl.dlopen(libflint)
        Libdl.dlopen(libarb)
+       Libdl.dlopen(libantic)
    else
       push!(Libdl.DL_LOAD_PATH, libdir)
    end
@@ -299,7 +301,7 @@ function __init__()
       (Ptr{Void},), cfunction(flint_abort, Void, ()))
 
    println("")
-   println("Welcome to Nemo version 0.8.0")
+   println("Welcome to Nemo version 0.8.2")
    println("")
    println("Nemo comes with absolutely no warranty whatsoever")
    println("")
@@ -320,7 +322,7 @@ end
 ################################################################################
 
 function versioninfo()
-  print("Nemo version 0.8.0\n")
+  print("Nemo version 0.8.2\n")
   nemorepo = dirname(dirname(@__FILE__))
 
   print("Nemo: ")
