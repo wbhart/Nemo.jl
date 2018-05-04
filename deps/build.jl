863b32deb2e5f20f78a6b29845dbf4bd43830702
Tommy Hofmann <thofma@gmail.com>
2018-05-04 11:38:48 +0200

    Bump arb version

--- deps/build.jl
+++ deps/build.jl
@@ -6,7 +6,7 @@ oldwdir = pwd()
 @show MPFR_VERSION = "4.0.0"
 @show ANTIC_VERSION = "7905f41b7ae661eef30c301f58dca4cd5dcf92cb"
 @show FLINT_VERSION = "adf1583c6bd92a454f3f92a18adf9063d14637a0"
-@show ARB_VERSION = "232135f35eeebb74afcea5dd7be436142bee9227"
+@show ARB_VERSION = "e0c823ab52c7a909acb692597864e748d73cdebe"
 
 pkgdir = dirname(dirname(@__FILE__))
 wdir = joinpath(pkgdir, "deps")
