005e0a3a0ff86ac215c4db86d3126d864d57686e
Édouard Rousseau <rousseau.edouard@gmail.com>
2018-05-14 15:42:58 +0200

    Suppression of the abstract file

--- src/FinFieldsLattices.jl
+++ src/FinFieldsLattices.jl
@@ -6,6 +6,64 @@
 
 export embed, section
 
+################################################################################
+#
+#  Finite fields morphism
+#
+################################################################################
+
+struct FinFieldMorphism{T} <: Map{T, T}
+    domain::T
+    codomain::T
+    f::Function
+    inv::Function
+end
+
+domain(f::FinFieldMorphism) = f.domain
+
+codomain(f::FinFieldMorphism) = f.codomain
+
+function (f::FinFieldMorphism)(x)
+    return f.f(x)::elem_type(codomain(f))
+end
+
+function Base.show(io::IO, f::FinFieldMorphism)
+    print("Morphism from $(domain(f))\nto $(codomain(f))")
+end
+
+struct FinFieldSection{T} <: Map{T, T}
+    domain::T
+    codomain::T
+    f::Function
+    inv::Function
+end
+
+domain(f::FinFieldSection) = f.domain
+
+codomain(f::FinFieldSection) = f.codomain
+
+function (f::FinFieldSection)(x)
+    a = f.f(x)::elem_type(codomain(f))
+    b = f.inv(a)
+    if x == b
+        return a
+    else
+        error("Element not in the domain")
+    end
+end
+
+function Base.show(io::IO, f::FinFieldSection)
+    print("Section from $(domain(f))\nto $(codomain(f))")
+end
+
+section(f::FinFieldMorphism) = FinFieldSection(f.codomain, f.domain, f.inv, f.f)
+
+################################################################################
+#
+#  Over/Sub fields
+#
+################################################################################
+
 overfields(k::FqNmodFiniteField) = k.overfields
 subfields(k::FqNmodFiniteField) = k.subfields
 
