b9cce4589334af89659c18deb89f71130dab8f5d
Tommy Hofmann <thofma@gmail.com>
2018-08-28 08:27:10 +0200

    Adjust docs/make.jl and .travis.yml

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,7 @@ using Documenter, Nemo
 makedocs(
          format   = :html,
          sitename = "Nemo.jl",
+         doctest = false,
          pages    = [
              "index.md",
              "about.md",
@@ -26,10 +27,9 @@ makedocs(
 )
 
 deploydocs(
-   julia = "nightly",
+   julia = "1.0",
    repo   = "github.com/Nemocas/Nemo.jl.git",
    target = "build",
-   deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
+   deps = nothing,
    make   = nothing
 )
-
