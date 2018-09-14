3eea5bc381a03d73cdcb1fa1acf37aa985037776
William Hart <goodwillhart@googlemail.com>
2018-09-14 13:03:54 +0200

    Add type for gfp_poly.

--- src/flint/FlintTypes.jl
+++ src/flint/FlintTypes.jl
@@ -414,8 +414,6 @@ struct gfp_elem <: FinFieldElem
    parent::GaloisField
 end
 
-const Zmodn = Union{nmod, gfp_elem}
-
 ###############################################################################
 #
 #   NmodPolyRing / nmod_poly
@@ -560,6 +558,153 @@ function _nmod_poly_factor_clear_fn(a::nmod_poly_factor)
   ccall((:nmod_poly_factor_clear, :libflint), Nothing,
           (Ref{nmod_poly_factor}, ), a)
 end
+
+################################################################################
+#
+#   GFPPolyRing / gfp_poly
+#
+###############################################################################
+
+mutable struct GFPPolyRing <: PolyRing{gfp_elem}
+  base_ring::GaloisField
+  S::Symbol
+  n::UInt
+
+  function GFPPolyRing(R::GaloisField, s::Symbol, cached::Bool = true)
+    m = UInt(modulus(R))
+    if haskey(GFPPolyRingID, (m, s))
+       return GFPPolyRingID[m, s]
+    else
+       z = new(R, s, m)
+       if cached
+          GFPPolyRingID[m, s] = z
+       end
+       return z
+    end
+  end
+end
+
+const GFPPolyRingID = Dict{Tuple{UInt, Symbol}, GFPPolyRing}()
+
+mutable struct gfp_poly <: PolyElem{gfp_elem}
+   coeffs::Ptr{Nothing}
+   alloc::Int
+   length::Int
+   mod_n::UInt
+   mod_ninv::UInt
+   mod_norm::UInt
+   parent::GFPPolyRing
+
+   function gfp_poly(n::UInt)
+      z = new()
+      ccall((:nmod_poly_init, :libflint), Nothing, (Ref{gfp_poly}, UInt), z, n)
+      finalizer(_gfp_poly_clear_fn, z)
+      return z
+   end
+
+   function gfp_poly(n::UInt, a::UInt)
+      z = new()
+      ccall((:nmod_poly_init, :libflint), Nothing, (Ref{gfp_poly}, UInt), z, n)
+      ccall((:nmod_poly_set_coeff_ui, :libflint), Nothing,
+              (Ref{gfp_poly}, Int, UInt), z, 0, a)
+      finalizer(_gfp_poly_clear_fn, z)
+      return z
+   end
+
+   function gfp_poly(n::UInt, a::Int)
+      z = new()
+      ccall((:nmod_poly_init, :libflint), Nothing, (Ref{gfp_poly}, UInt), z, n)
+      ccall((:nmod_poly_set_coeff_ui, :libflint), Nothing,
+              (Ref{gfp_poly}, Int, UInt), z, 0, mod(a, n))
+      finalizer(_gfp_poly_clear_fn, z)
+      return z
+   end
+
+   function gfp_poly(n::UInt, arr::Array{fmpz, 1})
+      z = new()
+      ccall((:nmod_poly_init2, :libflint), Nothing,
+            (Ref{gfp_poly}, UInt, Int), z, n, length(arr))
+      for i in 1:length(arr)
+         tt = ccall((:fmpz_fdiv_ui, :libflint), UInt, (Ref{fmpz}, UInt), arr[i], n)
+         ccall((:nmod_poly_set_coeff_ui, :libflint), Nothing,
+              (Ref{gfp_poly}, Int, UInt), z, i - 1, tt)
+      end
+      finalizer(_gfp_poly_clear_fn, z)
+      return z
+   end
+
+   function gfp_poly(n::UInt, arr::Array{UInt, 1})
+      z = new()
+      ccall((:nmod_poly_init2, :libflint), Nothing,
+            (Ref{gfp_poly}, UInt, Int), z, n, length(arr))
+      for i in 1:length(arr)
+         ccall((:nmod_poly_set_coeff_ui, :libflint), Nothing,
+              (Ref{gfp_poly}, Int, UInt), z, i - 1, arr[i])
+      end
+      finalizer(_gfp_poly_clear_fn, z)
+      return z
+   end
+
+   function gfp_poly(n::UInt, arr::Array{gfp_elem, 1})
+      z = new()
+      ccall((:nmod_poly_init2, :libflint), Nothing,
+            (Ref{gfp_poly}, UInt, Int), z, n, length(arr))
+      for i in 1:length(arr)
+         ccall((:nmod_poly_set_coeff_ui, :libflint), Nothing,
+              (Ref{gfp_poly}, Int, UInt), z, i-1, arr[i].data)
+      end
+      finalizer(_gfp_poly_clear_fn, z)
+      return z
+   end
+
+   function gfp_poly(n::UInt, f::fmpz_poly)
+      z = new()
+      ccall((:nmod_poly_init2, :libflint), Nothing,
+            (Ref{gfp_poly}, UInt, Int), z, n, length(f))
+      ccall((:fmpz_poly_get_nmod_poly, :libflint), Nothing,
+            (Ref{gfp_poly}, Ref{fmpz_poly}), z, f)
+      finalizer(_gfp_poly_clear_fn, z)
+      return z
+   end
+
+   function gfp_poly(n::UInt, f::gfp_poly)
+      z = new()
+      ccall((:nmod_poly_init2, :libflint), Nothing,
+            (Ref{gfp_poly}, UInt, Int), z, n, length(f))
+      ccall((:nmod_poly_set, :libflint), Nothing,
+            (Ref{gfp_poly}, Ref{gfp_poly}), z, f)
+      finalizer(_gfp_poly_clear_fn, z)
+      return z
+   end
+end
+
+function _gfp_poly_clear_fn(x::gfp_poly)
+  ccall((:nmod_poly_clear, :libflint), Nothing, (Ref{gfp_poly}, ), x)
+end
+
+mutable struct gfp_poly_factor
+  poly::Ptr{gfp_poly}  # array of flint nmod_poly_struct's
+  exp::Ptr{Int}
+  num::Int
+  alloc::Int
+  n::UInt
+
+  function gfp_poly_factor(n::UInt)
+    z = new()
+    ccall((:nmod_poly_factor_init, :libflint), Nothing,
+            (Ref{gfp_poly_factor}, ), z)
+    z.n = n
+    finalizer(_gfp_poly_factor_clear_fn, z)
+    return z
+  end
+end
+
+function _gfp_poly_factor_clear_fn(a::nmod_poly_factor)
+  ccall((:nmod_poly_factor_clear, :libflint), Nothing,
+          (Ref{gfp_poly_factor}, ), a)
+end
+
+const Zmodn_poly = Union{nmod_poly, gfp_poly}
 
 ###############################################################################
 #
