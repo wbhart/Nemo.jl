9ca0044c049e6e10751bf392fc37e0731ffa45f6
Tommy Hofmann <thofma@gmail.com>
2018-12-14 15:24:17 +0100

    Call this 0.11.0

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -237,7 +237,7 @@ function __init__()
          (Ptr{Nothing},), @cfunction(flint_abort, Nothing, ()))
 
    println("")
-   println("Welcome to Nemo version 0.10.3")
+   println("Welcome to Nemo version 0.11.0")
    println("")
    println("Nemo comes with absolutely no warranty whatsoever")
    println("")
@@ -258,7 +258,7 @@ end
 ################################################################################
 
 function versioninfo()
-  print("Nemo version 0.10.3\n")
+  print("Nemo version 0.11.0\n")
   nemorepo = dirname(dirname(@__FILE__))
 
   print("Nemo: ")
