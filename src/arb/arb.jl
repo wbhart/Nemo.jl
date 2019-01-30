adc2f756fca71bcad784b2b213e95ff492fc2ac1
Robo <sachinkm308@gmail.com>
2019-01-30 21:01:47 +0100

    * is replaced by ( which is right in this context

--- src/arb/arb.jl
+++ src/arb/arb.jl
@@ -1781,7 +1781,7 @@ numpart(n::Int, r::ArbField) = numpart(fmpz(n), r)
 @doc Markdown.doc"""
     lindep(A::Array{arb, 1}, bits::Int)
 > Find a small linear combination of the entries of the array $A$ that is small
-> *using LLL). The entries are first scaled by the given number of bits before
+> (using LLL). The entries are first scaled by the given number of bits before
 > truncating to integers for use in LLL. This function can be used to find linear
 > dependence between a list of real numbers. The algorithm is heuristic only and
 > returns an array of Nemo integers representing the linear combination.  
