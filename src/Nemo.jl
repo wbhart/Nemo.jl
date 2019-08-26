06a666198820ccceef31d87e5d1c7a5ca50c0fc9
thofma <thofma@gmail.com>
2019-08-26 15:44:27 +0200

    Update Nemo.jl

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -267,7 +267,7 @@ function __init__()
          (Ptr{Nothing},), @cfunction(flint_abort, Nothing, ()))
 
    println("")
-   println("Welcome to Nemo version 0.14.1")
+   println("Welcome to Nemo version 0.14.2")
    println("")
    println("Nemo comes with absolutely no warranty whatsoever")
    println("")
@@ -307,7 +307,7 @@ end
 ################################################################################
 
 function versioninfo()
-  print("Nemo version 0.14.1\n")
+  print("Nemo version 0.14.2\n")
   nemorepo = dirname(dirname(@__FILE__))
 
   print("Nemo: ")
