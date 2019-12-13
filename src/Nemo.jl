d5536857489608ac58df95928d6442053b308ea0
Tommy Hofmann <thofma@gmail.com>
2019-12-13 16:34:27 +0100

    Forgot to update version()

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -295,7 +295,7 @@ end
 #
 ################################################################################
 
-version() = v"0.15.2-dev"
+version() = v"0.16.0-dev"
 
 function versioninfo()
   print("Nemo version $(version())\n")
