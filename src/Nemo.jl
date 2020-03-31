842c904346384ceb53f321a8cc907a04441b34b3
Claus Fieker <fieker@mathematik.uni-kl.de>
2020-03-31 15:27:39 +0200

    add ?Nemo

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -1,3 +1,10 @@
+@doc Markdown.doc"""
+  Nemo is a computer algebra package for the Julia programming language, maintained by William Hart, Tommy Hofmann, Claus Fieker and Fredrik Johansson with additional code by Oleksandr Motsak and other contributors.
+
+ The Nemo code written in Julia is licensed under the BSD license and it makes use of GPL and LGPL C/C++ libraries such as Flint, Antic, GMP/MPIR, MPFR, Singular and Arb.
+
+ For more information please visit: https://nemocas.org
+"""
 module Nemo
 
 using AbstractAlgebra
