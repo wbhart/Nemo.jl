103a9f78f16fed122f4779767d32ec7be2bca774
Max Horn <max@quendi.de>
2020-12-18 12:17:09 +0100

    Deploy documentation to website using GitHub Actions (#930)
    * Deploy documentation to website using GitHub Actions
    
    * Upgrade to Documenter.jl v0.26
    
    * Ensure doctest breakage results in CI failures

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,13 @@
 using Documenter, Nemo, AbstractAlgebra
 
 makedocs(
-         format   = :html,
+         format = Documenter.HTML(),
          sitename = "Nemo.jl",
          modules = [Nemo, AbstractAlgebra],
          clean = true,
          checkdocs = :none,
-         doctest = false,
+         doctest = true,
+         strict = true,
          pages    = [
              "index.md",
              "about.md",
@@ -34,10 +35,6 @@ makedocs(
 )
 
 deploydocs(
-   julia = "1.0",
    repo   = "github.com/Nemocas/Nemo.jl.git",
-   target = "build",
-   deps = nothing,
-   make   = nothing,
-   osname = "linux"
+   target = "build"
 )
