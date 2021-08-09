b6985a8c22682909aabd253ab6840d1d58893e62
William Hart <goodwillhart@googlemail.com>
2021-08-09 12:16:33 +0200

    Add additional test.

--- test/flint/gfp-test.jl
+++ test/flint/gfp-test.jl
@@ -19,6 +19,7 @@
 
    @test isa(R(fmpq(7, 11)), Nemo.gfp_elem)
    @test R(fmpq(7, 11))*11 == 7
+   @test_throws ErrorException R(fmpq(7, 13))
 
    for i = 1:1000
       p = rand(UInt(1):typemax(UInt))
