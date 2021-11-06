4cd05c555939ec2bf206a965ce56217c1133cb8d
Tommy Hofmann <thofma@gmail.com>
2021-11-06 17:46:40 +0100

    Bump Flint version (#1199)

--- deps/build.jl
+++ deps/build.jl
@@ -25,6 +25,8 @@
     push!(dependencies, "build_FLINT.v200.800.0.jl")
   elseif flint_ver == v"200.800.100+0"
     push!(dependencies, "build_FLINT.v200.800.100.jl")
+  elseif flint_ver == v"200.800.300+0"
+    push!(dependencies, "build_FLINT.v200.800.300.jl")
   else
     throw(error("Flint version $flint_ver not supported for julia version <= 1.3"))
   end
