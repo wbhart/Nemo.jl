43d0a26decc58abbd0e815ce01d2786d2d794ea1
William Hart <goodwillhart@googlemail.com>
2022-01-26 15:49:05 +0100

    Add sqrt functions for gfp_fmpz_abs_series.

--- src/flint/fmpz_mod_abs_series.jl
+++ src/flint/fmpz_mod_abs_series.jl
@@ -737,6 +737,90 @@ end # for
 
 ###############################################################################
 #
+#   Square root
+#
+###############################################################################
+
+function sqrt_classical_char2(a::gfp_fmpz_abs_series; check::Bool=true)
+   S = parent(a)
+   asqrt = S()
+   prec = div(precision(a) + 1, 2)
+   asqrt = set_precision!(asqrt, prec)
+   if check
+     for i = 1:2:precision(a) - 1 # series must have even exponents
+       if !iszero(coeff(a, i))
+         return false, S()
+       end
+     end
+   end
+   for i = 0:prec - 1
+     asqrt = setcoeff!(asqrt, i, coeff(a, 2*i))
+   end
+   return true, asqrt
+end
+ 
+function sqrt_classical(a::gfp_fmpz_abs_series; check::Bool=true)
+   R = base_ring(a)
+   S = parent(a)
+   if characteristic(R) == 2
+      return sqrt_classical_char2(a; check=check)
+   end
+   v = valuation(a)
+   z = S()
+   z.prec = a.prec - div(v, 2)
+   if iszero(a)
+      return true, z
+   end
+   if check && !iseven(v)
+      return false, S()
+   end
+   a = shift_right(a, v)
+   c = coeff(a, 0)
+   if check
+      flag, s = issquare_with_sqrt(c)
+      if !flag
+         return false, S()
+      end
+   else
+      s = sqrt(c; check=check)
+   end
+   a = divexact(a, c)
+   z.prec = a.prec - div(v, 2)
+   ccall((:fmpz_mod_poly_sqrt_series, libflint), Nothing,
+         (Ref{gfp_fmpz_abs_series}, Ref{gfp_fmpz_abs_series},
+              Int, Ref{fmpz_mod_ctx_struct}),
+                z, a, a.prec, a.parent.base_ring.ninv)
+   if !isone(s)
+      z *= s
+   end
+   if !iszero(v)
+      z = shift_left(z, div(v, 2))
+   end
+   return true, z
+end
+ 
+@doc Markdown.doc"""
+   sqrt(a::gfp_fmpz_abs_series; check::Bool=true)
+ 
+Return the power series square root of $a$.
+"""
+function Base.sqrt(a::gfp_fmpz_abs_series; check::Bool=true)
+   flag, s = sqrt_classical(a; check=check)
+   check && !flag && error("Not a square")
+   return s
+end
+  
+function issquare(a::gfp_fmpz_abs_series)
+   flag, s = sqrt_classical(a; check=true)
+   return flag
+end
+ 
+function issquare_with_sqrt(a::gfp_fmpz_abs_series)
+   return sqrt_classical(a; check=true)
+end
+
+###############################################################################
+#
 #   PowerSeriesRing constructor
 #
 ###############################################################################
